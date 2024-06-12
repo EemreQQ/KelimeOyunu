cbaf02f10514e58fb3bdf9d48da5fd37ced1c767
emre erol <emre_erol17@hotmail.com>
2024-06-12 20:37:45 +0300

    yeniAdd project files.

---
+++ KelimeOyunu/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+
+namespace KelimeOyunu
+{
+    public partial class Form1 : Form
+    {
+        int start = 0;
+        string s = null;
+        char[,] Matrix = new char[15, 15];
+
+        Random r = new Random();
+        List<Button> buttonsList = new List<Button>();
+        SortedDictionary<char, int> Harfadet = new SortedDictionary<char, int>();
+        char c = ' ';
+        int count = 0;
+        string rndmKelime = "";
+        int rndadet = 0;
+        bool IsTrue = false;
+        int buttonSayisi = 0;
+
+        // Kelime listesi
+        List<string> istenenKelimeler = new List<string>();
+        // İstenen kelimelerden rastgele tutulan isim/kelime random harflerimle oluşturuluyor mu?
+        Dictionary<char, int> RndHarfadet = new Dictionary<char, int>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            StreamReader streamReader = new StreamReader("C:\\Users\\emre_\\Desktop\\isimler.txt");
+            while (!streamReader.EndOfStream)
+            {
+                string str = streamReader.ReadLine();
+                if(!istenenKelimeler.Contains(str))
+                    istenenKelimeler.Add(str);
+            }
+            
+            string harfler = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ";
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    // Random bir harf tut
+                    c = harfler[r.Next(0, harfler.Length)];
+                    Matrix[i, j] = c;
+
+
+                    if (Harfadet.ContainsKey(c))
+                    { 
+                        if (c.ToString() == "Ğ" && Harfadet['Ğ'] >= 2) // 2 den fazla yumuşak g'ye gerek yok
+                        {
+                            j--; continue;
+                        }
+                        Harfadet[c]++;// O harften var mı? Evet,adedini arttır.
+                    }
+                    else Harfadet.Add(c, 1); // Hayır yok,ekle.
+                    
+                    Button b = new Button(); // Yeni Buton oluştur
+                    b.Text = c.ToString(); //
+                    b.Size = new Size(27, 27);
+                    b.Location = new Point((j+1) * 27, (i) * 27);
+                    b.BackColor= Color.Tan;
+                    b.MouseDown += Button_MouseDown;
+                    this.Controls.Add(b);
+                }
+            }
+
+            
+            for (int i = 0; listBox1.Items.Count<12; i++)
+            {
+                rndmKelime = istenenKelimeler[r.Next(0, istenenKelimeler.Count)];
+                // Rastgele kelime seçildi.
+                for (int j = 0; j < rndmKelime.Length; j++)
+                {
+                    if (!RndHarfadet.ContainsKey(rndmKelime[j]))
+                        RndHarfadet.Add(rndmKelime[j], 1);
+                    else RndHarfadet[rndmKelime[j]]++;
+                }
+                // Rastgele seçilen kelimenin harfleri sayıldı
+
+                // Bu kadar harf var mı diye iki Dictionarydeki değerlere bakıyoruz..
+                foreach (var item in RndHarfadet)
+                {
+                    rndadet = item.Value;
+                    IsTrue = true;
+                    if (!Harfadet.ContainsKey(item.Key) || Harfadet[item.Key] <= rndadet)
+                    {
+                        IsTrue = false;
+                        RndHarfadet.Clear();
+                        break;
+                    }                  
+                }
+                foreach (var ch in RndHarfadet)
+                {
+                    Harfadet[ch.Key] -= ch.Value;
+                }
+
+                if (IsTrue && !listBox1.Items.Contains(rndmKelime)) { listBox1.Items.Add(rndmKelime.ToString());}
+                RndHarfadet.Clear();
+            }
+           
+            void Button_MouseDown(object sender, MouseEventArgs e)
+            {
+                Button button = sender as Button;
+                if (button.BackColor == Color.Yellow)
+                {
+                    button.BackColor = SystemColors.Control;
+                    s=s.Remove(buttonsList.Count- 1);
+                    buttonsList.Remove(button);
+                }
+                else 
+                { 
+                    buttonsList.Add(button);
+                    s += buttonsList[buttonsList.Count - 1].Text.ToString();
+                    button.BackColor = Color.Yellow;
+                }
+
+                DogruKelime();
+
+            }   
+            
+            bool DogruKelime()
+            {
+                if (istenenKelimeler.Contains(s)) // Eklenilen harfle birlikte mantıklı bir kelime oluştu mu?
+                {
+                    {
+                        MessageBox.Show("Bir kelime buldunuz");
+                        start = 0;
+                        listBox1.Items.RemoveAt(listBox1.Items.IndexOf(s));
+                        if (listBox1.Items.Count == 0) MessageBox.Show("Tebrikler,daha fazla bulunacak kelime yok.");
+                        s = null;
+
+                        for (int j = 0; j < buttonsList.Count; j++)
+                        {
+                            buttonsList[j].MouseDown -= Button_MouseDown;
+                            buttonsList[j].BackColor = Color.Gray;
+                        }
+                        buttonsList.Clear();
+                        return true;
+                    }
+                }
+                return false;
+             
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
